407b2bb405346625c58d1f807dd7d37c603ad7d1
tess022095 <47835932+tess022095@users.noreply.github.com>
2023-01-12 02:29:47 -0600

    Update bank_XGB_DI.py

--- evaluation/bank/bank_XGB_DI.py
+++ evaluation/bank/bank_XGB_DI.py
@@ -225,7 +225,7 @@ def accuracy(solution, prediction):
 
     if os.stat("beta.txt").st_size == 0:
 
-        default = XGBClassifier(learning_rate = 0.3, n_estimator = 100, max_depth=6, subsample=1, min_child_weight=1)
+        default = XGBClassifier()
         degrees = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
         mutation_strategies = {"0": [1, 0], "1": [0, 1]}
         dataset_orig = subset_data_orig_train
